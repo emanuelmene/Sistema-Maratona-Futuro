cc988c02f5b649efcf87e1823958e6bdd71018c0
emanuelmene <menezeswebdeveloper@gmail.com>
2023-09-05 04:40:24 -0500

    Cria Rota do login para o sistema

--- validacaoLogin.js
+++ validacaoLogin.js
@@ -1,3 +1,13 @@
-function login(){
-    window.location.href = "sistema.html";
-}
+document.getElementById("aluno").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById("emailLogin").value;
+    const senha = document.getElementById("senhaLogin").value;
+
+    if (email === "emanuel@123" && senha === "123") {
+        // Redireciona para a página "sistema.html" após o login bem-sucedido
+        window.location.href = "sistema.html";
+    } else {
+        alert("Login falhou. Verifique suas credenciais.");
+    }
+});
